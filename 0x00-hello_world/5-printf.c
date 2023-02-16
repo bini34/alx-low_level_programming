db7b3e6d07e831c6ab32969b45bf7b99e79b040e
bini34 <biniyamambachew@gmail.com>
2023-02-16 12:01:01 -0800

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio>
+/**
+ * this code will print in printf function have
+ * int main function will be excted print f function
+ * return (0) sucess
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
