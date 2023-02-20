9ea1dfd561d1e592de25145897fd6b7445e5fbbc
bini34 <biniyamambachew@gmail.com>
2023-02-20 09:24:49 -0800

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,17 @@
-#!/bin/bash
-#include<unistd.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
- * Description: print a quote using write function
- *    ssize_t write(int fd, const void *buf, size_t count);
+ * Description: Prints a quote to the standard error.
  *
- * Return: This time we return an error 1.
-*/
-
+ * Return: Always 1 (Error).
+ */
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, quo, 59);
+	write(2, quote, 59);
 	return (1);
+}
 }
