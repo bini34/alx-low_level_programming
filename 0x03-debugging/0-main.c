10cbee4898a68ae78d16d4b66320aa3ea518a85b
bini34 <biniyamambachew@gmail.com>
2023-02-24 15:18:20 -0800

    add task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+* main - fuction call if posetive or negative
+* Return: 0 (susessfully)
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 98 % 2;
+	positive_or_negative(i);
+
+	return (0);
+}
