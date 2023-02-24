438fa24a16933324971dfe527ef4f051481cc18c
bini34 <biniyamambachew@gmail.com>
2023-02-24 13:27:27 -0800

    update task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,14 +4,14 @@
  * @size: The size of the square to be printed.
  */
 void print_square(int size)
-{
+{ int i,j;
     if (size <= 0)
     {
         _putchar('\n');
         return;
     }
 
-    int i, j;
+    
     for (i = 0; i < size; i++)
     {
         for (j = 0; j < size; j++)
