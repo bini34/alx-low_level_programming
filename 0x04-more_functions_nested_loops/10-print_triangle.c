16b68b4f915548f6a7549f0d1f14addcf2540087
bini34 <biniyamambachew@gmail.com>
2023-02-24 13:18:59 -0800

    update task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,14 +4,14 @@
  * @size: The size of the triangle to be printed.
  */
 void print_triangle(int size)
-{
+{ int i,j;
     if (size <= 0)
     {
         _putchar('\n');
         return;
     }
 
-    int i, j;
+    
     for (i = 1; i <= size; i++)
     {
         for (j = 1; j <= size - i; j++)
