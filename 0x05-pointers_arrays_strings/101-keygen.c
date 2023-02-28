1ea0452bacb204619c17d9d263817d3b2820e7cf
bini34 <biniyamambachew@gmail.com>
2023-02-28 12:19:11 -0800

    update task  11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -36,6 +36,15 @@ int main(void)
 				break;
 			}
 		}
+
+		for (index =0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+                        {
+                                password[index] -= diff_half2;
+                                break;
+                        }
+                }
 	}
 	printf("%s", password);
 	return (0);
