e257f6ea1de7a2c46bb9270047c9615efbcd997d
bini34 <biniyamambachew@gmail.com>
2023-03-09 10:54:59 -0800

    update task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,19 +9,21 @@
 
 int _sqrt_recursion(int n)
 {
+	int i;
+
 	if (n < 0)
 		return (-1);
 
 	if (n == 0 || n == 1)
 		return (n);
 
-	int i;
 
 	for (i = 1; i <= n; i++)
 	{
-		if (i  i == n)
+		if (i == n)
 			return (i);
-		if (i  i > n)
+		if (i > n)
 			return (-1);
 	}
+	return (-1);
 }
