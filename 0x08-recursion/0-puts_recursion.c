fa3fd4c4457d083f8e31b614c2b2e934bc1226ae
bini34 <biniyamambachew@gmail.com>
2023-03-09 10:21:51 -0800

    update task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
- *  _print_rev_recursion - a function that prints a new line
+ * _puts_recursion - a function that prints a new line
  *
  * @s: the string
  *
  * Return: nothing
-*/
-void  _print_rev_recursion(char *s)
+ */
+void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
+		_putchar('\n');
 		return;
 	}
-	_print_rev_recursion(s + 1);
-	putchar(*s);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
