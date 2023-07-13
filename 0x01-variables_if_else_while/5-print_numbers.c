2a9bc82f6f5c55ee02fe3944388d0056abbffece
root <root@BINI>
2023-07-14 02:36:55 +0300

    add task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters except q and e
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%d", x);
+		x++;
+	}
+	return (0);
+}
