7ef379c124b037370419c84b4b86f97adb63ece9
root <root@BINI>
2023-07-13 03:14:22 +0300

    update task 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  *
  * Return: Always 0 (Success)
 */
-int main()
+int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
