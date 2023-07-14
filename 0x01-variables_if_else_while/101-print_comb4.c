654ad7e36c87cbd157c95281f7134d02d7128906
root <root@BINI>
2023-07-14 06:28:57 +0300

    update task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,7 +21,7 @@ int main(void)
 			num3 = 0;
 			while (num3 <= 9)
 			{
-				if (num1 != num2 && num1 < num2 && num2 != num3 && num2 < num3 )
+				if (num1 != num2 && num1 < num2 && num2 != num3 && num2 < num3)
 				{
 					putchar(num1 + 48);
 					putchar(num2 + 48);
