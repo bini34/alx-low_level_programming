b19b6dd42176ca1c449d7a5116de61526776f3f3
root <root@BINI>
2023-07-18 14:00:17 +0300

    add task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - a function that prints the last
+ *			digit of a number
+ * @n: a number that use
+ *
+ * Return: Returns the value of the last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
