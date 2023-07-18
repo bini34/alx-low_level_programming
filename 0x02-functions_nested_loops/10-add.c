810f626ccda2b1e1e71de2cf15fa2168e8ed34a8
root <root@BINI>
2023-07-18 20:54:37 +0300

    add task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - a function that adds two integers and returns the result
+ *
+ * @x: first intiger
+ * @y: second intiger
+ *
+ * Return: the sum of two int
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
