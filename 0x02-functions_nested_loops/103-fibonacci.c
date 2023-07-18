3268c1ffbb279ef3acd05c68a00ccec60874c99f
root <root@BINI>
2023-07-18 23:44:55 +0300

    add task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ *        separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long i, x = 1, y = 2, z, sum = 0 ;
+
+	for (i = 1; i <= 4000000 ; i++)
+	{
+		z = x + y;
+		if( (z % 2) == 0)
+			sum += z;
+		x = y;
+		y = z;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
