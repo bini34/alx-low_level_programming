1581e589d1645445860cb4c303ff2d1a6936e571
root <root@BINI>
2023-07-18 13:43:51 +0300

    add task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolute value of an integer
+ *
+ * @int: a number that use
+ *
+ * Return: the absolute value of an integer
+ */
+int _abs(int)
+{
+	return (abs(int));
+}
