35d22c1d5e92845518145a7bc31c9e49443ef59e
root <root@BINI>
2023-07-18 13:31:47 +0300

    add task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - function to determine if
+ *            a number is posetive or negative or zero
+ *
+ * @n: character to be checked
+ *
+ * Return: 1 if 'n' is greater than zero ,
+ * 		-1 if 'n'  is less than zero,
+ *         otherwise 0 (success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
