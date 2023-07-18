4f0d524da2933440b9cb738c04fc7f1d7005b2df
root <root@BINI>
2023-07-18 23:47:35 +0300

    update task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	unsigned long i, x = 1, y = 2, z, sum = 0 ;
+	unsigned long i, x = 1, y = 2, z, sum = 0;
 
 	for (i = 1; i <= 4000000 ; i++)
 	{
 		z = x + y;
-		if( (z % 2) == 0)
+		if ((z % 2) == 0)
 			sum += z;
 		x = y;
 		y = z;
