48d1158f0ce1267d1b4c5886b014526c7a401619
root <root@BINI>
2023-07-18 11:48:44 +0300

    add task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always O (Success)
+*/
+int main(void)
+{
+	char str[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+	return (0);
+}
+
