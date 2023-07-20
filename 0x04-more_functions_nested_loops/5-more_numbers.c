1a8db2f6fa6bf76987e727f73b475b2a09022d0c
root <root@BINI>
2023-07-20 13:11:53 +0300

    update task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,7 +4,7 @@
  */
 void more_numbers(void)
 {
-	int i, j, k, l;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
@@ -12,7 +12,7 @@ void more_numbers(void)
 		{
 			if (j > 9)
 				_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0')
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
