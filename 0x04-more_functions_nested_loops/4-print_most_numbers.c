e585dff68aecc8669c1aa4c1497cbdb78b24e66e
root <root@BINI>
2023-07-20 12:50:14 +0300

    add task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - a function that prints the numbers, from 0 to 9
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+			i++;
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
