06a5e213f3d46e009bae98a8f5b76bb527e3f9c5
root <root@BINI>
2023-07-20 12:16:53 +0300

    add task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - a function that checks for uppercase characte
+ * @c: a charcter that check
+ * Return: return 1 if c is uppercase or 0 other
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+		return (1);
+	else
+		return (0);
+}
