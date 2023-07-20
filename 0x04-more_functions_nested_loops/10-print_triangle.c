50dd5862086b66d22660a8e00cc694e299bb4658
root <root@BINI>
2023-07-20 18:28:46 +0300

    update

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,22 +4,24 @@
  * @size: The size of the triangle to be printed.
  */
 void print_triangle(int size)
-{ int i,j;
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+{
+	int i, j;
 
-    
-    for (i = 1; i <= size; i++)
-    {
-        for (j = 1; j <= size - i; j++)
-            _putchar(' ');
-
-        for (j = 1; j <= i; j++)
-            _putchar('#');
-
-        _putchar('\n');
-    }
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size - i; j++)
+		{
+			_putchar(' ');
+		}
+		for (j = 1; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
 }
