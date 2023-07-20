05784343cb1f98bff27bb896eac28e7361922165
root <root@BINI>
2023-07-20 18:17:43 +0300

    add task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include"main.h"
+/**
+ * print_triangle - Prints a triangle of a given size in the terminal.
+ * @size: The size of the triangle to be printed.
+ */
+void print_triangle(int size)
+{ int i,j;
+    if (size <= 0)
+    {
+        _putchar('\n');
+        return;
+    }
+
+    
+    for (i = 1; i <= size; i++)
+    {
+        for (j = 1; j <= size - i; j++)
+            _putchar(' ');
+
+        for (j = 1; j <= i; j++)
+            _putchar('#');
+
+        _putchar('\n');
+    }
+}
