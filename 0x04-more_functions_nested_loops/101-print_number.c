4be740b011e6d55d7bb0c758f5332f63bf89694a
root <root@BINI>
2023-07-20 19:11:57 +0300

    update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,8 +14,8 @@ void print_number(int n)
 		_putchar('-');
 	}
 	else
-		n1 = n;
-	if (n1 / 10)
-		print_number(n1 / 10);
-	_putchar((n1 % 10) + '0');
+		num = n;
+	if (num / 10)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
 }
