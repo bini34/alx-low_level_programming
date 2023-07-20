27f8a17f73f2d655acaddd22a9dd1b3c751fdbc9
root <root@BINI>
2023-07-20 13:26:22 +0300

    update task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,9 +10,13 @@ void print_line(int n)
 	for (i = 1; i <= n; i++)
 	{
 		if (n <= 0)
-		       _putchar('\n');
+		{
+			_putchar('\n');
+		}
 		else
-			_putchar('_');	
+		{
+			_putchar('_');
+		}
 	}
 	_putchar('\n');
 }
