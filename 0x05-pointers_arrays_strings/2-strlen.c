43b02a64a7e95ca3ca1b50cd47cd7e2bf8ea1b5f
root <root@BINI>
2023-07-24 11:32:07 +0300

    add task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: a string that check
+ * Return: returns the length of a string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
