8e7eef514447ab510358a5803eb1c62f0efdaf24
root <root@BINI>
2023-07-24 11:40:05 +0300

    add task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _puts - a function that prints a string
+ *	followed by a new line, to stdout
+ * @str: a charcter that print
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
