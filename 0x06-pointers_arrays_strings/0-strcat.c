1716afa23524679ac96e154721b842456de05ca1
root <root@BINI>
2023-07-26 11:36:41 +0300

    add task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcat - a function that concatenates two strings
+ *
+ * @dest: the string will be append to
+ * @src: the string will be from
+ * Return: Returns a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
