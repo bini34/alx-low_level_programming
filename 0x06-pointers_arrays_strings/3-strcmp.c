672d5e03268b0bae7b2008dc51b33f34fd680e39
root <root@BINI>
2023-07-26 12:13:07 +0300

    add task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcmp - a function that compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: compare value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
