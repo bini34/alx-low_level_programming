7234ea0fc9a1f031cace240fc037852e0f11dcc5
root <root@BINI>
2023-07-28 01:38:08 +0300

    uppdate task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,22 +1,24 @@
 #include "main.h"
 /**
- * leet -  encode into 1337speak
- * @str: input value
- * Return: n value`:wq
- */
-char *leet(char *str)
+* leet - encode into 1337speak
+* @n: input value
+* Return: n value
+*/
+char *leet(char *n)
 {
-	int i, j;
-	char letter[] = "a,A,e,E,o,O,t,T,i,I";
-	char repletter[] = "4,4,3,3,0,0,7,7,1,1";
+int i, j;
+char s1[] = "aAeEoOtTlL";
+char s2[] = "4433007711";
 
-	for (i = 0 ; str[i] != '\0'; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			if (str[i] == letter[j])
-				str[i] = repletter[j];
-		}
-	}
-	return (str);
+for (i = 0; n[i] != '\0'; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (n[i] == s1[j])
+{
+n[i] = s2[j];
+}
+}
+}
+return (n);
 }
