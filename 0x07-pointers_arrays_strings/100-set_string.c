83b2ccaa4a040a0ba6406f285a6268e09f391b02
root <root@BINI>
2023-07-31 17:35:50 +0300

    add task 9

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string -  function that sets the value of a pointer to a char
+ * @s: second string
+ * @to: first string
+ */
+void set_string(char **s, char *to)
+{
+		*s = to;
+}
