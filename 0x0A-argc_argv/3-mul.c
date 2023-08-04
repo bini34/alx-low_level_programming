1fffef72eedf60e9b0e3e4d6a722fbf5edab5df5
root <root@BINI>
2023-08-04 11:07:33 +0300

    add task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * main - program that prints its name
+ * @ac: argument counter
+ * @av: argument variable
+ * Return: return 0
+ */
+int main(int ac, char *av[])
+{
+	int i;
+	int mul = 1;
+
+	if (ac == 1)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	for (i = 1; i < ac; i++)
+	{
+		mul *= atoi(av[i]);
+	}
+	printf("%d\n", mul);
+
+	return (0);
+}
+
