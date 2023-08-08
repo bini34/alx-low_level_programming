0a2e12bd3057b9c468bce8d0f98a7f6b38e42a22
root <root@BINI>
2023-08-08 11:04:10 +0300

    update task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -17,7 +17,7 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-		total_len += 2 + strlen(av[i]);
+		total_len += 1 + strlen(av[i]);
 	}
 	ptr = (char *) malloc(total_len * sizeof(char));
 	if (ptr == NULL)
