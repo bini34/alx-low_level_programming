da9d842170860326f12ef964db965feff937d562
root <root@BINI>
2023-08-10 09:34:07 +0300

    add task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _realloc - reallocate a memory block using malloc and free
+ *
+ * @ptr: pointer to previous memory
+ * @old_size: old memory array size
+ * @new_size: new memory array size
+ *
+ * Return: pointer to new memory
+ *         NULL if it fails
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+		return (malloc(new_size));
+
+	if (new_size == old_size)
+		return (ptr);
+
+	new_ptr = malloc(new_size);
+
+	if (new_ptr == NULL)
+		return (NULL);
+
+	memcpy(new_ptr, ptr, (old_size < new_size) ? old_size : new_size);
+	free(ptr);
+	return (new_ptr);
+}
+
+
