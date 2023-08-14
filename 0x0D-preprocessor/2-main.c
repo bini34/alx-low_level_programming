2eb4dc4111436f718ca928e456ff890a2fdaff2b
root <root@BINI>
2023-08-14 23:40:50 +0300

    update task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * @ac: argument counter
- * @args: argument
  * Return: return 0
  */
 int main(void)
 {
-	printf ("%s", __FILE__);
-	return(0);
+	printf("%s", __FILE__);
+	return (0);
 }
