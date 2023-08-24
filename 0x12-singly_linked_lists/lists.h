11e1d2807326fcad4094053128dc1f4e13847a90
bini34 <biniyamambachew@gmail.com>
2023-08-24 22:24:32 +0300

    uppdate task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -14,10 +19,8 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
-#include <stdio.h>
-#include <stddef.h>
-#include <stdlib.h>
-#include <string.h>
+
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
