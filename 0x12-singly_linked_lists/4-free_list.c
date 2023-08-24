db8ba4c08013ba9516ab4ce6c9346299c3c364bb
bini34 <biniyamambachew@gmail.com>
2023-08-24 19:32:01 +0300

    add task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - a function that frees a list_t list
+ * @head: a first node pointer
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
