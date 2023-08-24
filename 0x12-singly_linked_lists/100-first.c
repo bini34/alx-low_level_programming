a450dfa10605fa7a834cb26039104dc099d03004
bini34 <biniyamambachew@gmail.com>
2023-08-24 19:48:21 +0300

    add task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include"lists.h"
+/**
+ * my_constructor -  a function that prints before the
+ *				main function is executed.
+ */
+void my_constructor(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
