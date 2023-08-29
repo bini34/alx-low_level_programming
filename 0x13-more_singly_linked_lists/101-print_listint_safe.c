e9bcf6c87a03dc3d9577a504e447e6e3d2960d88
bini34 <biniyamambachew@gmail.com>
2023-08-30 02:39:55 +0300

    update task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -13,8 +13,6 @@ size_t print_listint_safe(const listint_t *head)
 	const listint_t *current = head;
 	const listint_t *next;
 
-	if (head == NULL)
-		exit(98);
 	while (current != NULL)
 	{
 		count++;
