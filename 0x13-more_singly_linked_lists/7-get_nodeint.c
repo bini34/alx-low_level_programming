cff522ea0622260d62bf00365a63ae668e187cc2
bini34 <biniyamambachew@gmail.com>
2023-08-29 19:40:03 +0300

    update task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp;
 
 	if (head == NULL)
-		return (NULL);
+		return (0);
 	temp = head;
 	for (i = 0; i < index; i++)
 	{
