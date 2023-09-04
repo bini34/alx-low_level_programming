5bfcddcba742a8f7a93fb9c68ad611b65599afef
bini34 <biniyamambachew@gmail.com>
2023-09-04 23:16:55 +0300

    uppdate task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,15 +17,22 @@ int main(int argc, char **argv)
 	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]), exit(97);
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(EXIT_FAILURE);
+	}
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
 	if (fd_to == -1)
 	{
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), close(fd_from);
-	exit(99);	}
+	close(fd_from);
+	exit(EXIT_FAILURE);	}
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
@@ -34,7 +41,7 @@ int main(int argc, char **argv)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
-			exit(99);
+			exit(EXIT_FAILURE);
 		}
 	}
 	if (bytes_read == -1)
@@ -42,14 +49,16 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
 		close(fd_to);
-		exit(98);	}
+		exit(EXIT_FAILURE);	}
 	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		close(fd_to);
-		exit(100);	}
+		exit(EXIT_FAILURE);	}
 	if (close(fd_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(EXIT_FAILURE);
+	}
 	return (0);
 }
-
