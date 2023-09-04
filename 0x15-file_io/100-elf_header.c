fcef9ddb6bf4af7dc8b8e08e6563f26df99d4c0b
bini34 <biniyamambachew@gmail.com>
2023-09-04 23:37:55 +0300

    uppdate task 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,46 +1,66 @@
+#include "main.h"
+#include <elf.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 
-#define ELF_MAGIC 0x7f454c46
+/**
+ * main - check the code
+ * description: displays the information contained in
+ * the ELF header at the start of an ELF file
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success, 98 on failure
+ */
+int main(int argc, char *argv[])
+{
+	int fd;
+	Elf64_Ehdr *header;
 
-int main(int argc, char *argv[]) {
-	  unsigned char elf_header[64];
-	    FILE *fp;
+	if (argc != 2)
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n"), exit(98);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]), exit(98);
 
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-    exit(1);
-  }
+	header = malloc(sizeof(Elf64_Ehdr));
+	if (header == NULL)
+		dprintf(STDERR_FILENO, "Error: malloc failed\n"), exit(98);
+	if (read(fd, header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 
-  fp = fopen(argv[1], "rb");
-  if (fp == NULL) {
-    perror("fopen");
-    exit(1);
-  }
+	if (header->e_ident[EI_MAG0] != ELFMAG0 ||
+			header->e_ident[EI_MAG1] != ELFMAG1 ||
+			header->e_ident[EI_MAG2] != ELFMAG2 ||
+			header->e_ident[EI_MAG3] != ELFMAG3)
+		dprintf(STDERR_FILENO, "Error: Not an ELF file\n"), exit(98);
 
-  if (fread(elf_header, 1, sizeof(elf_header), fp) != sizeof(elf_header)) {
-    perror("fread");
-    exit(1);
-  }
-
-
-  if (elf_header[0] != ELF_MAGIC) {
-    fprintf(stderr, "Not an ELF file\n");
-    exit(1);
-  }
-
-  printf("ELF Header:\n");
-  printf("  Magic: 0x%08x\n", elf_header[0] | (elf_header[1] << 8) | (elf_header[2] << 16) | (elf_header[3] << 24));
-  printf("  Class: %d\n", elf_header[4]);
-  printf("  Data: %d\n", elf_header[5]);
-  printf("  Version: %d\n", elf_header[6] | (elf_header[7] << 8));
-  printf("  OS/ABI: %d\n", elf_header[8] | (elf_header[9] << 8));
-  printf("  ABI Version: %d\n", elf_header[10] | (elf_header[11] << 8));
-  printf("  Type: %d\n", elf_header[12] | (elf_header[13] << 8));
-  printf("  Entry point address: 0x%08x\n", elf_header[14] | (elf_header[15] << 8) | (elf_header[16] << 16) | (elf_header[17] << 24));
-
-  fclose(fp);
-  return 0;
+	printf("ELF Header:\n");
+	printf("  Magic:   %02x %02x %02x %02x\n",
+			header->e_ident[EI_MAG0], header->e_ident[EI_MAG1],
+			header->e_ident[EI_MAG2], header->e_ident[EI_MAG3]);
+	printf("  Class:                             %s\n",
+			header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
+	printf("  Data:                              %s\n",
+			header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+	printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
+	printf("  OS/ABI:                            %s\n",
+			header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "UNIX - System V");
+	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+	printf("  Type:                              %s\n",
+			header->e_type == ET_EXEC ? "EXEC (Executable file)" : "EXEC (Executable file)");
+	printf("  Entry point address:               0x%lx (bytes into file)\n", header->e_entry);
+	printf("  Start of program headers:          %d (bytes into file)\n", header->e_phoff);
+	printf("  Start of section headers:          %d (bytes into file)\n", header->e_shoff);
+	printf("  Flags:                             0x%x\n", header->e_flags);
+	printf("  Size of this header:               %d (bytes)\n", header->e_ehsize);
+	printf("  Size of program headers:           %d (bytes)\n", header->e_phentsize);
+	printf("  Number of program headers:         %d\n", header->e_phnum);
+	printf("  Size of section headers:           %d (bytes)\n", header->e_shentsize);
+	printf("  Number of section headers:         %d\n", header->e_shnum);
+	printf("  Section header string table index: %d\n", header->e_shstrndx);
+	if (close(fd) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(98);
+	return (0);
 }
-
