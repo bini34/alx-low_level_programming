0b1c891d2e552c693e46d6d48285e342d8fe3c8a
bini34 <biniyamambachew@gmail.com>
2023-09-04 16:26:26 +0300

    add task 0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
