bf00c1e6177a490ceac29ab84efd2d0c3cd61cff
bini34 <biniyamambachew@gmail.com>
2023-11-13 22:48:10 +0300

    hacking

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,31 +1,3 @@
 #!/bin/bash
-
-# Upload the hack.so library
-cp hack.so .
-
-# Wait for 98 seconds
-sleep 98
-
-# Remove the shell script
-rm 101-make_me_win.sh
-
-# List the directory contents
-ls -la
-
-# Clear command history
-history -c
-
-# Clear the terminal
-clear
-
-# List the directory contents again
-ls -la
-
-# Display the MD5 checksum of gm
-md5sum gm
-
-# Run the gm program
-./gm 9 8 10 24 75 9
-
-# Exit the session
-exit
+wget -P .. https://raw.githubusercontent.com/bini34/alx-low_level_programming/main/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD="$PWD/../libhack.so"
